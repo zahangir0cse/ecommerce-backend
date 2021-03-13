d91ce12c91d4da5f53f0552d30eac452037b5a18
Zahangir Alam <zahangir0cse@gmail.com>
2021-03-13 21:42:53 +0600

    First commit

---
+++ src/main/java/com/zahjava/ecommercebackend/EcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zahjava.ecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackendApplication.class, args);
+    }
+
+}
